a40624bb66b934bd5ce928631821053402f69c43
Paulo Prado <paulroar@hotmail.com>
2025-04-04 13:38:26 +0100

    April 04.7 - New Footer - socials

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/UserContext'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css'
 import App from './App.jsx'
 
