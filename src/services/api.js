10ecb8554bcdfa7a461c2d37d2b43a3893024299
Paulo Prado <paulroar@hotmail.com>
2025-04-05 09:31:15 +0100

    Fix: add auth token to GET requests

--- src/services/api.js
+++ src/services/api.js
@@ -13,19 +13,33 @@ const api = axios.create({
 const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 const api = {
-  get: (endpoint) =>
+  get: (endpoint, options = {}) =>
     fetch(`${BASE_URL}${endpoint}`, {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        ...options.headers,
       },
     }).then(res => res.json()),
+
   post: (endpoint, data) =>
     fetch(`${BASE_URL}${endpoint}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(data),
+    }).then(res => res.json()),
+
+  put: (endpoint, data, options = {}) =>
+    fetch(`${BASE_URL}${endpoint}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        ...options.headers,
       },
       body: JSON.stringify(data),
     }).then(res => res.json()),
