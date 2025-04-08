d9678369c80bab0e7ae504f06a85cfc21d5ce20a
Paulo Prado <paulroar@hotmail.com>
2025-04-08 01:35:47 +0100

    Fix: styles Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
     <UserProvider>
       <BrowserRouter>
         <App />
-        <ToastContainer position="top-center" autoClose={1000} transition={Zoom} />
+        <ToastContainer position="top-center" autoClose={850} transition={Zoom} />
       </BrowserRouter>
     </UserProvider>
     </CartProvider>
